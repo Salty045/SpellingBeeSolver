7beb65bbbda2c2fc726930d90a7f7cfdacf1917d
Salty045 <jahankaz1126@gmail.com>
2024-05-17 17:39:17 -0700

    version 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String str = "bcejdot";
-        char c = 't';
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter all 7 letters:");
+        String str = input.nextLine();
+        System.out.println("Enter the required letter");
+        char c = input.next().charAt(0);
         ArrayList<String> words = SpellingSolver.compWord(str,c);
-        System.out.println(words);
+
+        for(int i = 0; i<words.size(); i++){
+            System.out.print(words.get(i) +", ");
+            if(i % 35 == 0 && i != 0){
+                System.out.println();
+            }
+        }
 
         ArrayList<String> pans = SpellingSolver.getPangrams(str,c);
-        System.out.println(pans);
+
+        System.out.println("\n\n"+ pans);
     }
 }
